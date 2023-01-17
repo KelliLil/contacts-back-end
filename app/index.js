e12604ce9a55efca97623a0c67c143d91c4acea8
Kelli Lilly <kelli.lilly05@gmail.com>
2023-01-17 11:58:26 -0600

    Add example of fake update with put

--- app/index.js
+++ app/index.js
@@ -3,19 +3,49 @@
 
 import express from "express";
 
+const CONTACTS = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Jane" },
+];
+
 const app = express();
 
-app.get("/", (_, res) => {
-  res.send("<h1>Hello World</h1>");
+app.get("/api/contacts", (_, response) => {
+  response.json(CONTACTS);
 });
 
-app.get("/about", (_, res) => {
-  res.send("<h1> About </h1>");
+// ':id' is a route parameter
+app.get("/api/contacts/:id", (request, response) => {
+  const { id } = request.params;
+  const contact = CONTACTS.find((contact) => contact.id === Number(id));
+
+  if (contact) {
+    response.json(contact);
+  } else {
+    response.status(404).json({ message: "contact not found" });
+  }
 });
 
 // Express uses middleware to parse body of request
 // This is required to access the body of a POST request
 app.use(express.json());
+
+app.put("/api/contacts/:id", (request, response) => {
+  const { id } = request.params;
+  const updatedContact = request.body;
+
+  // Use map to update contact
+  const updatedContacts = CONTACTS.map((contact) => {
+    if (contact.id === Number(id)) {
+      console.log("updating");
+      return updatedContact;
+    }
+
+    return contact;
+  });
+
+  response.json(updatedContacts);
+});
 
 app.post("/api/contacts", (request, response) => {
   console.log(request.body);
